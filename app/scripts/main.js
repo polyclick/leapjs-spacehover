80df389a566ae3500218aabf8eafffd7592b4e3f
Bart Claessens <bart.cla@gmail.com>
2015-10-16 00:00:14 +0200

    planet motion

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,6 +4,7 @@ function Application() {
   this.camera = null;
   this.scene = null;
   this.renderer = null;
+  this.planet = null;
   this.object = null;
   this.frame = null;
 
@@ -11,8 +12,7 @@ function Application() {
   this.dirLight = null;
 
   this.objectRotation = {x:0, y:90 * Math.PI / 180, z:0};
-  this.objectQuaternion = new THREE.Quaternion();
-
+  this.objectAcceleration = {x:0, y:0, z:0};
 
   this.init();
   this.run();
@@ -63,8 +63,23 @@ Application.prototype = {
     //this.dirLight.shadowDarkness = 0.35;
     this.scene.add( this.dirLight );
 
+    this.planet = this.createPlanet();
+    this.planet.position.y = 750;
+    this.scene.add(this.planet);
+
     //this.createCube();
     this.createSpaceCraft();
+  },
+
+  createPlanet: function() {
+    return new THREE.Mesh(
+      new THREE.SphereGeometry(850, 32, 32),
+      //new THREE.MeshBasicMaterial( { color:'#ff0000', wireframe:true } )
+      new THREE.MeshPhongMaterial({
+        map: THREE.ImageUtils.loadTexture('images/planet.jpg'),
+        side: THREE.DoubleSide
+      })
+    );
   },
 
   createCube: function() {
@@ -127,6 +142,9 @@ Application.prototype = {
 
   animate: function() {
     this.processLeapFrame(this.frame);
+
+    this.planet.rotation.x -= 0.01 + (1 * Math.PI / 180 * this.objectAcceleration.x);
+    //this.planet.rotation.y += 0.005 + (1 * Math.PI / 180 * this.objectAcceleration.y);
   },
 
   handleHandFound: function() {
@@ -162,15 +180,30 @@ Application.prototype = {
     var str = "";
     for (var i in frame.handsMap) {
       var hand = frame.handsMap[i];
-      str += "<strong>Roll:</strong> " + hand.roll() +
-        "<br/><strong>Pitch:</strong> " + hand.pitch() +
-        "<br/><strong>Yaw:</strong> " + hand.yaw();
 
-      TweenMax.to(this.objectRotation, 0.25, {
-        x: hand.pitch() * 25 * Math.PI / 180,
-        z: hand.roll() * 25 * Math.PI / 180,
+      // correct resting hand
+      var rollCorrected = hand.roll() + 0.2362;
+      var pitchCorrected = hand.pitch() - 0.3189;
+      var yawCorrected = hand.yaw() + 0.1786;
+
+
+      str += "<strong>Roll:</strong> " + rollCorrected +
+        "<br/><strong>Pitch:</strong> " + pitchCorrected +
+        "<br/><strong>Yaw:</strong> " + yawCorrected;
+
+      TweenMax.to(this.objectRotation, 1, {
+        x: (15 + (pitchCorrected * 35)) * Math.PI / 180,
+        y: ((90 + (yawCorrected * -25) ) * Math.PI / 180),
+        z: rollCorrected * 35 * Math.PI / 180,
         onUpdate: this.updateSpaceCraftRotation.bind( this )
       });
+
+      this.objectAcceleration.x = (pitchCorrected + 0.3) * -1;
+      //this.objectAcceleration.y = (pitchCorrected + 0.3) * -1;
+
+      // camera
+      TweenMax.to(this.camera, 1, {fov:75 + ( Math.max(0, pitchCorrected * -30) ), onUpdate:function(){ this.camera.updateProjectionMatrix(); }.bind(this)});
+
     }
 
 
@@ -179,15 +212,25 @@ Application.prototype = {
   },
 
   updateSpaceCraftRotation: function() {
-    var quaternionX = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(1, 0, 0), this.objectRotation.x);
-    var quaternionY = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(0, 1, 0), this.objectRotation.y);
-    var quaternionZ = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(0, 0, 1), this.objectRotation.z);
+    var q = this.calculateQuarternion(this.objectRotation);
+    this.object.setRotationFromQuaternion(q);
+  },
+
+  updatePlanetRotation: function() {
+    var q = this.calculateQuarternion(this.planetRotation);
+    this.object.setRotationFromQuaternion(q);
+  },
+
+  calculateQuarternion: function(objectRotation) {
+    var quaternionX = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(1, 0, 0), objectRotation.x);
+    var quaternionY = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(0, 1, 0), objectRotation.y);
+    var quaternionZ = new THREE.Quaternion().setFromAxisAngle( new THREE.Vector3(0, 0, 1), objectRotation.z);
 
     var quaternion = quaternionZ;
     quaternion = quaternion.multiplyQuaternions(quaternion, quaternionX);
     quaternion = quaternion.multiplyQuaternions(quaternion, quaternionY);
 
-    this.object.setRotationFromQuaternion(quaternion);
+    return quaternion;
   },
 
   render: function() {
